d575312a902566f744e11109c1a6de435402ae4e
Esmurzelly <78688219+Esmurzelly@users.noreply.github.com>
2022-08-30 12:20:28 +0300

    full_adaptive_menu

--- js/script.js
+++ js/script.js
@@ -122,11 +122,7 @@ btn.addEventListener("click", function(e) {
     if(email.validity.valid && name.validity.valid && subject.validity.valid && phone.validity.valid && checkbox.checked) {        
         modal.style.display = 'block'; 
         body.classList.toggle("noscroll");
-    }
-    
-   // body.style.overflow = "hidden";
-
-    
+    }    
 });
 window.addEventListener("click", function(e) {
     if(e.target == modal) {
@@ -140,9 +136,7 @@ window.addEventListener("click", function(e) {
         checkbox.checked = false;
 
         body.classList.remove("noscroll");
-    //body.style.overflow = "visible";
     }
-    //
 
 }, false)
 
@@ -217,4 +211,50 @@ if(menuLinks.length > 0) {
                 }
             })
     })
+};
+
+
+// menu for phones
+const isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    Blackberry: function() {
+        return navigator.userAgent.match(/Blackberry/i);
+    },
+    IOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (
+            isMobile.Android() ||
+            isMobile.Blackberry() ||
+            isMobile.IOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows()
+        );
+    },
+};
+
+if(isMobile.any()) {
+    document.body.classList.add("_touch");
+
+    let menuArrows = document.querySelectorAll(".triangle_down");
+    if(menuArrows.length > 0) {
+        for(let index = 0; index < menuArrows.length; index++){
+            const menuArrow = menuArrows[index];
+            menuArrow.addEventListener("click", function(e) {
+                menuArrow.parentElement.classList.toggle("_active");
+            });
+        }
+    } else {
+        document.body.classList.add("pc");
+    }
 }
+
